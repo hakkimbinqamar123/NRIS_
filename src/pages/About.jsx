0e3f7b213c664958c73d5bf56a862c7b09be1ee5
Abdul Hakeem M K <151653465+hakkimbinqamar123@users.noreply.github.com>
2026-09-17 18:54:58 +0530

    commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -46,11 +46,11 @@ export default function About() {
     <div className="relative min-h-screen">
       <AnimatedBackground />
       {/* HERO */}
-      <section className="relative overflow-hidden w-full h-[calc(100dvh-81px)]">
+      <section className="relative overflow-hidden w-full md:h-[calc(100dvh-81px)] bg-black/5">
         <motion.img loading="lazy"
           src={aboutHeroImg}
           alt="About NRiS Hero"
-          className="w-full h-full object-cover object-top z-0"
+          className="w-full h-auto md:h-full object-cover object-top z-0 block"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.8 }}
